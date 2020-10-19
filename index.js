133999b9988ac90ed60fdfabda36602be39f85b7
JaronArmiger <jaron.armiger@gmail.com>
2020-10-19 17:30:09 -0500

    write index.js for express app

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ const generatePassword = require('password-generator');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  const passwords = Array.from(Array(count).keys()).map(i => {
+  	return generatePassword(12, false);
+  });
+
+  res.json(passwords);
+
+  console.log(`sent ${count} passwords`);
+});
+
+// for any request that doesn't match one above
+// send back React's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
